0286e270a0db1e1e13a2dc3f0acd12db455c644c
Engr. Ademola yusuf <95187506+cresslinktech@users.noreply.github.com>
2026-04-30 10:51:06 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   name: 'Humanity CSS',
   tagline: 'Specialist Domiciliary Care',
   phone: '07877 200 381',
-  email: 'info@hcssconnect.co.uk',
+  email: 'humanitycss@gmail.com',
   address: 'London, United Kingdom',
   cqcRating: 'GOOD',
 };
